deefca5c62beb0c6703e257531bf0a95f967ac98
p1oxer <pabonna@mail.ru>
2025-04-08 20:33:18 +0300

    intro no adaptive

--- js/script.js
+++ js/script.js
@@ -3,113 +3,108 @@ document.querySelector(".icon-menu").addEventListener("click", () => {
     document.body.classList.toggle("lock");
     document.body.classList.toggle("menu-open");
 });
-document.querySelector(".menu-icon-close").addEventListener("click", () => {
-    document.body.classList.remove("lock");
-    document.body.classList.remove("menu-open");
-});
+
 //scrollTo =========
-document.querySelectorAll('a[href^="#"').forEach((link) => {
-    link.addEventListener("click", function (e) {
-        e.preventDefault();
+// document.querySelectorAll('a[href^="#"').forEach((link) => {
+//     link.addEventListener("click", function (e) {
+//         e.preventDefault();
 
-        let href = this.getAttribute("href").substring(1);
+//         let href = this.getAttribute("href").substring(1);
 
-        const scrollTarget = document.getElementById(href);
+//         const scrollTarget = document.getElementById(href);
 
-        const topOffset = 30;
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
+//         const topOffset = 30;
+//         const elementPosition = scrollTarget.getBoundingClientRect().top;
+//         const offsetPosition = elementPosition - topOffset;
 
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: "smooth",
-        });
-    });
-});
+//         window.scrollBy({
+//             top: offsetPosition,
+//             behavior: "smooth",
+//         });
+//     });
+// });
 
+// // tabs ==========
+// const tabButtons = document.querySelectorAll(".body-sertificates__buttons-item");
+// const tabContents = document.querySelectorAll(".body-sertificates__item");
 
-// tabs ==========
-const tabButtons = document.querySelectorAll(".body-sertificates__buttons-item");
-const tabContents = document.querySelectorAll(".body-sertificates__item");
+// if (tabButtons && tabContents) {
+//     tabButtons.forEach((tabBtn) => {
+//         tabBtn.addEventListener("click", () => {
+//             const tabId = tabBtn.getAttribute("data-id");
 
-if (tabButtons && tabContents) {
-    tabButtons.forEach((tabBtn) => {
-        tabBtn.addEventListener("click", () => {
-            const tabId = tabBtn.getAttribute("data-id");
+//             tabButtons.forEach((btn) => btn.classList.remove("active"));
+//             tabBtn.classList.add("active");
 
-            tabButtons.forEach((btn) => btn.classList.remove("active"));
-            tabBtn.classList.add("active");
+//             tabContents.forEach((content) => {
+//                 content.classList.remove("active");
 
-            tabContents.forEach((content) => {
-                content.classList.remove("active");
-
-                if (content.id === tabId) {
-                    content.classList.add("active");
-                }
-            });
-        });
-    });
-}
+//                 if (content.id === tabId) {
+//                     content.classList.add("active");
+//                 }
+//             });
+//         });
+//     });
+// }
 
 // showmore ==========
-const certificatesShowMore = document.querySelector(".body-sertificates__showmore");
+// const certificatesShowMore = document.querySelector(".body-sertificates__showmore");
 
-const hiddenElementsCertificates = document.querySelectorAll(
-    ".body-sertificates__buttons-item.hidden"
-);
+// const hiddenElementsCertificates = document.querySelectorAll(
+//     ".body-sertificates__buttons-item.hidden"
+// );
 
-function showMore(elementList, event) {
-    const isHidden = elementList[0].classList.contains("hidden");
+// function showMore(elementList, event) {
+//     const isHidden = elementList[0].classList.contains("hidden");
 
-    elementList.forEach((element) => {
-        element.classList.toggle("hidden");
-    });
+//     elementList.forEach((element) => {
+//         element.classList.toggle("hidden");
+//     });
 
-    event.target.textContent = isHidden ? "Скрыть" : "Показать ещё";
-}
+//     event.target.textContent = isHidden ? "Скрыть" : "Показать ещё";
+// }
 
-certificatesShowMore.addEventListener("click", function (event) {
-    showMore(hiddenElementsCertificates, event);
-});
-
+// certificatesShowMore.addEventListener("click", function (event) {
+//     showMore(hiddenElementsCertificates, event);
+// });
 
 // modals =====
 
-const modalButtons = document.querySelectorAll(".modal-open");
-const modal = document.querySelector(".modal-request");
+// const modalButtons = document.querySelectorAll(".modal-open");
+// const modal = document.querySelector(".modal-request");
 
-modalButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        new Fancybox(
-            [
-                {
-                    src: modal,
-                    type: "html",
-                },
-            ],
-            {
-                closeButton: false,
-                hideScrollbar: false,
-            }
-        );
-    });
-});
+// modalButtons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         new Fancybox(
+//             [
+//                 {
+//                     src: modal,
+//                     type: "html",
+//                 },
+//             ],
+//             {
+//                 closeButton: false,
+//                 hideScrollbar: false,
+//             }
+//         );
+//     });
+// });
 
-const modalDoneButtons = document.querySelectorAll(".next-popup");
-const modalDone = document.querySelector(".modal-done");
-modalDoneButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        new Fancybox(
-            [
-                {
-                    src: modalDone,
-                    type: "html",
-                },
-            ],
-            {
-                closeButton: false,
-                hideScrollbar: false,
-            }
-        );
-    });
-});
+// const modalDoneButtons = document.querySelectorAll(".next-popup");
+// const modalDone = document.querySelector(".modal-done");
+// modalDoneButtons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         new Fancybox(
+//             [
+//                 {
+//                     src: modalDone,
+//                     type: "html",
+//                 },
+//             ],
+//             {
+//                 closeButton: false,
+//                 hideScrollbar: false,
+//             }
+//         );
+//     });
+// });
--- js/swipers.js
+++ js/swipers.js
@@ -1,5 +1,6 @@
-const introSwiper = new Swiper(".body-intro__slider", {
+const introSwiper = new Swiper(".body-intro__slider-main", {
     spaceBetween: 10,
+    speed: 1500,
     navigation: {
         prevEl: ".body-intro__swiper-btn-prev",
         nextEl: ".body-intro__swiper-btn-next",
@@ -8,66 +9,19 @@ const introSwiper = new Swiper(".body-intro__slider", {
         el: ".intro-pagination",
         clickable: true,
     },
-    autoplay: {
-        delay: 3000,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
     },
-    loop: true,
 });
-const aboutSwiper = new Swiper(".body-about__slider", {
+const introSwiperSide = new Swiper(".body-intro__slider-side", {
     spaceBetween: 10,
-    navigation: {
-        prevEl: ".body-about__swiper-btn-prev",
-        nextEl: ".body-about__swiper-btn-next",
-    },
-    pagination: {
-        el: ".about-pagination",
-        clickable: true,
+    speed: 1500,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
     },
 });
 
-const reviewsSwiper = new Swiper(".reviews__slider", {
-    slidesPerView: 2,
-    navigation: {
-        prevEl: ".reviews__swiper-btn-prev",
-        nextEl: ".reviews__swiper-btn-next",
-    },
-
-    pagination: {
-        el: ".reviews-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-        },
-        767: {
-            spaceBetween: 15,
-        },
-        992: {
-            spaceBetween: 30,
-        },
-    },
-});
-const worksSwiper = new Swiper(".works__slider", {
-    slidesPerView: 2,
-    navigation: {
-        prevEl: ".works__swiper-btn-prev",
-        nextEl: ".works__swiper-btn-next",
-    },
-
-    pagination: {
-        el: ".works-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-        },
-        600: {
-            spaceBetween: 15,
-        },
-        992: {
-            spaceBetween: 30,
-        },
-    },
-});
+introSwiper.controller.control = introSwiperSide;
+introSwiperSide.controller.control = introSwiper;
